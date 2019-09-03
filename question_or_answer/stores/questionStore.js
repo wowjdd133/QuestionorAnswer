195632249dd5d799e5335b8e9498fdbcd01261f6
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2019-09-03 20:21:40 +0900

    [ADD] questionStore, questionitem, questionList
    스토어 반 완성, 로직 추가?

--- question_or_answer/stores/questionStore.js
+++ question_or_answer/stores/questionStore.js
@@ -5,7 +5,13 @@ class QuestionStore {
 
   @action
   putQuestion = (question) => {
-    this.questions.push({question});
+    console.log(Object.keys(question)[0]);
+    this.questions.push({question});//this.questions.question.tag로 해야하는 불편함이 있음.
+    console.log(this.questions[0].question.tag);
+  }
+
+  getQuestion = () => {
+    console.log(this.questions[0]);
   }
 }
 
